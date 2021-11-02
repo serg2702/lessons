422f9bce48d82ba6f155b91def938283d3541e32
serg <serg2002st@gmail.com>
2021-11-02 17:30:37 +0300

    lesson16 end

--- js/script.js
+++ js/script.js
@@ -67,4 +67,51 @@ for (let i = 1; i < 8; i++) {
     // Урок14 конец*/
 
 
+/*let num = 20; // Глобальная переменная
+
+function showFirtsMessage(text) { // Может использовать глобальные переменные
+    console.log(text);
+    let num = 10; // Локальная переменная
+    console.log(num); // Сначала будет локальная, потом, если её нет, то глобальная
+}
+
+showFirtsMessage("Hello world");
+console.log(num);
+
+
+console.log(calc(4, 3)); // Можно сначала вызвать функцию, а потом объявлять её (не будет ошибки)
+console.log(calc(5, 6));
+console.log(calc(10, 3));
+
+function calc(a, b) {
+    return (a + b); // После return функция заканчивается
+}
+
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 3));
+
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+
+const logger = function() {
+    console.log("Hello");
+}; // В таком случае обязательно точка с запятой. Такая функция создаётся только когда доходит код
+    // Её нельзя сначала вызвать, а потом объявить
+logger();
+
+
+const calcc = (a, b ) => { 
+    return a + b;
+};
+    // Урок16 конец*/
+
+
 
