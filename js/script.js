ae1f8a45e43acf3f147fb0cfafa4e8f754fb534d
serg <serg2002st@gmail.com>
2021-11-02 16:16:26 +0300

    lesson 14 end

---
+++ js/script.js
@@ -0,0 +1,70 @@
+"use strict";
+
+/*if (4 == 9) {
+    console.log('ok');
+} else {
+    console.log('error');
+}
+
+if (1) { // Бесконечный цикл
+    console.log(true);
+}
+
+const num = 50;
+if (num < 49) {
+    console.log('error');
+} else if (num > 100) {
+    console.log('Too much');
+} else {
+    console.log('ok');
+}
+
+
+const numb = 50;
+(numb === 50) ? console.log('ok') : console.log('error');
+
+
+const a = 50; // Так же работает со строками ''
+switch (a) {
+    case 49:
+        console.log('neverno');
+        break;
+    case 100:
+        console.log('neverno');
+        break;
+    case 50:
+        console.log('verno');
+        break;
+    default:
+        console.log('ne v etot raz');
+        break;
+}
+    // Урок13 конец
+
+
+let  n = 50;
+
+while (n <= 55) {
+    console.log(n);
+    n++;
+}
+
+do {
+    console.log(n);
+    n++;
+}
+while (n < 55);
+
+let f = 50;
+for (let i = 1; i < 8; i++) {
+    if (i === 6) {
+        //break; Полное прерывание цикла
+        continue; // Пропуск одного шага цикла
+    }
+
+    console.log(i);
+}
+    // Урок14 конец*/
+
+
+console.log('11');
