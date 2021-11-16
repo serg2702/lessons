06fa4ca9e4808289026b0ff1c34e9118e27015e3
serg <serg2002st@gmail.com>
2021-11-16 22:58:59 +0300

    lesson26 end

--- js/script.js
+++ js/script.js
@@ -435,4 +435,61 @@ mark.sayHello();*/
     //Урок23 конец
 
 
+//  Динамическая типизация (Изменение типа в другой (строка -> число и др.))
+
+// To String
+
+// 1 Способ (старый)
+console.log(typeof(String(null)));
+console.log(String(4)); // Выводит цифру как строку
+
+
+// 2 Способ
+console.log(typeof(null + '')); // При сложении со строкой получается строка
+
+const num = 5;
+console.log("https://vk.com/catalog/" + num); // Примеры использования
+
+const fontSize = 26 + 'px';
+
+
+
+// To Number
+
+// 1 Способ (старый)
+console.log(typeof(Number('4')));
+
+
+// 2 Способ
+console.log(typeof(+'5'));
+
+
+// 3 Способ
+console.log(typeof(parseInt("15px", 10)));
+
+let answer = +prompt("hello", ""); // Пример использования
+
+
+
+// To boolean
+// 0, '', null undefined, NaN -- всегда false
+
+// 1 Способ
+let switcher = null;
+switcher = 1;
+if (switcher) {
+    console.log('working...');
+}
+
+
+// 2 Способ
+console.log(typeof(Boolean('4')));
+
+
+// 3 Способ
+console.log(typeof(!!"43gg")); // Два !! = boolean
+
+    //Урок26 конец
+
+
 
